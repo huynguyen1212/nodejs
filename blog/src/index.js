3f70a63c1f037cb50b65119c90d5bdeda2ba1195
huynguyen1212 <huyro1212000@gmail.com>
2021-10-07 00:21:07 +0700

    update rout, form

--- blog/src/index.js
+++ blog/src/index.js
@@ -8,6 +8,11 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname, "public")))
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 //HTTP loger
 app.use(morgan('combined'))
 
@@ -23,7 +28,15 @@ app.get('/', (req, res) => {
 })
 
 app.get('/news', (req, res) => {
-  res.render('home');
+  res.render('news');
+})
+
+app.get('/search', (req, res) => {
+  res.render('search');
+})
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send("");
 })
 
 app.listen(port, () => {
