2011cda201e61485a2c3e3144ab11fa2381fb6eb
huynguyen1212 <huyro1212000@gmail.com>
2021-10-14 00:38:12 +0700

    update delete

--- blog/src/app/models/Course.js
+++ blog/src/app/models/Course.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const slug = require('mongoose-slug-generator');
-
-mongoose.plugin(slug);
+const mongooseDelete = require('mongoose-delete');
 
 const Course = new Schema({
   name: { type: String, required: true, maxlength: 255 },
@@ -15,5 +14,9 @@ const Course = new Schema({
 }, {
   timestamps: true
 });
+
+//add plugins
+mongoose.plugin(slug);
+Course.plugin(mongooseDelete, { overrideMethods: 'all', deletedAt: true });
 
 module.exports = mongoose.model('Course', Course);
